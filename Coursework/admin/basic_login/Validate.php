3135f25c96811ef0931262afc18f9b4e2a7a123c
phongttgcs220282 <phongttgcs220282@fpt.edu.vn>
2025-12-05 13:32:21 +0700

    Update Validate.php

--- Coursework/admin/basic_login/Validate.php
+++ Coursework/admin/basic_login/Validate.php
@@ -1,16 +1,28 @@
 <?php
+session_start();
+
 $ActualEmail = "phongttgcs220282@fpt.edu.vn";
 $ActualPassword = "secret";
 
-    if ($_POST["email"] === $ActualEmail && $_POST["password"] === $ActualPassword) {
+if (isset($_POST["email"]) && isset($_POST["password"])) {
 
-        session_start();
+    $email = trim($_POST["email"]);
+    $password = trim($_POST["password"]);
+
+    if ($email === $ActualEmail && $password === $ActualPassword) {
+
         $_SESSION["Authorised"] = "Y";
+
         header("Location: ../index.php");
         exit();
-        
+
     } else {
         header("Location: Wrongpassword.php");
         exit();
     }
+
+} else {
+    header("Location: ../index.php?error=missing_fields");
+    exit();
+}
 ?>
